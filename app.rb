5e9c8938f9d875202a3f10559325a74980ec6551
Steven Brooks <brooks.stevena@gmail.com>
2013-06-28 08:46:30 -0400

    finished the homework

--- app.rb
+++ app.rb
@@ -1,9 +1,16 @@
 require 'sinatra/base'
+require 'debugger'
 
 module SuperSinatra
   class App < Sinatra::Base
 
     get '/' do
+      erb :super_hero
+    end
+
+    post '/team' do
+      @team = params[:team]
+      erb :team
     end
 
   end
